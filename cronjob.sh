4c5d64e8f98573419ad6fcfc5c6bdccd88b87bb3
Kevin <kevin2kyoto@gmail.com>
2018-01-15 00:10:26 -0500

    removed dead code and whitespace

--- cronjob.sh
+++ cronjob.sh
@@ -6,63 +6,57 @@ fi
 
 touch tmp.txt
 
-curl -s "$url" 2> /dev/null > tmp.txt
 
 echo "Current Login Name" >> info.txt
 logname         >> info.txt
 
-echo "\nlines, words, bytes" >> info.txt
-wc -cwl tmp.txt >> info.txt
-
-echo "\nDay, Month, Time, Timezone, Year" >> info.txt
+echo "Day, Month, Time, Timezone, Year" >> info.txt
 date            >> info.txt
 
-echo "\n Free Space " >> info.txt
+echo "Free Space " >> info.txt
 df -aH          >> info.txt
 
-echo "\n%JOBID, PID, Status, Name" >> info.txt
+echo "%JOBID, PID, Status, Name" >> info.txt
 jobs            >> info.txt
 
-echo "\nLast 10 commands" >> info.txt
+echo "Last 10 commands" >> info.txt
 history | tail  >> info.txt
 
-echo "\nDisk Utility of ." >> info.txt
+echo "Disk Utility of ." >> info.txt
 du -sh          >> info.txt
 
-echo "\nCurrent Environment" >> info.txt
+echo "Current Environment" >> info.txt
 env             >> info.txt
 
-echo "\nActivity Monitor" >> info.txt
+echo "Activity Monitor" >> info.txt
 top -u | head   >> info.txt
 
-echo "\nCPU and io statistics" >> info.txt
+echo "CPU and io statistics" >> info.txt
 iostat          >> info.txt
 
-echo "\nDU Quota" >> info.txt
+echo "DU Quota" >> info.txt
 quota -q        >> info.txt
 
-echo "\nFS status" >> info.txt
-stat -xtql      >> info.txt  
+echo "FS status" >> info.txt
+stat -xtql      >> info.txt
 
-echo "\nUptime" >> info.txt
+echo "Uptime" >> info.txt
 uptime          >> info.txt
 
-echo "\nCurrent Users" >> info.txt
+echo "Current Users" >> info.txt
 users           >> info.txt
 who             >> info.txt
 
-echo "\nList . Symlinks" >> info.txt
+echo "List . Symlinks" >> info.txt
 find . -maxdepth 1 -type l -ls >> info.txt
 
-echo "\nRunning Processes" >> info.txt
+echo "Running Processes" >> info.txt
 ps gv           >>info.txt
 
 if [ $? -gt 0 ]; then
     echo "Bad Job Status"
-    > info.txt 
+    > info.txt
 fi
 
-echo "\n" >> info.txt
+echo >> info.txt
 rm tmp.txt
-
-exit 0
